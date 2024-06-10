002a3915ebee4aeef2668836515073fd8c1e03ed
vlauncher <v2dent@gmail.com>
2024-06-10 13:02:30 +0100

    updates

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,7 @@
 import express,{ Application } from 'express';
 import { config } from 'dotenv';
+import swaggerUI from 'swagger-ui-express'
+import * as swaggerDocument from '../swagger.json'; 
 
 // dotenv config
 config();
@@ -8,6 +10,9 @@ const app:Application = express()
 
 app.use(express.json());
 app.use(express.urlencoded({ extended:false }));
+
+// Serve Swagger UI documentation
+app.use('/api', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 // Routes setup
 import usersRoutes from './routes/users';
